d199c43733afcf9f5ea5f17c57fdbc575c752589
Curry Xu <jialincurryxu@gmail.com>
2019-11-13 19:07:10 -0800

    Twine + both scenes
    Added the Twine story into the unity project and a buttom to transfer from the story to the game

---
+++ MagicTrickPrototype/Assets/fade.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fade : MonoBehaviour
+{
+    public string scene;
+    public Color loadToColor = Color.white;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnGUI() {
+            if (GUI.Button(new Rect(Screen.width / 2 - 50, (2 * Screen.height) / 3, 100, 30), "Start")) {
+                Initiate.Fade(scene, loadToColor, 0.5f);
+            }
+    }
+}
